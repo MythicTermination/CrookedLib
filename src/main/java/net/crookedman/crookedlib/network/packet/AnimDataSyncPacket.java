0bbac1f92d868974cb589635c5a620716b3e0b43
MythicTermination <157101314+MythicTermination@users.noreply.github.com>
2025-02-14 11:44:07 -0600

    Add files via upload

---
+++ src/main/java/net/crookedman/crookedlib/network/packet/AnimDataSyncPacket.java
@@ -0,0 +1,58 @@
+package net.crookedman.crookedlib.network.packet;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraftforge.network.NetworkEvent;
+import net.crookedman.crookedlib.animatable.SingletonGeoAnimatable;
+import net.crookedman.crookedlib.constant.DataTickets;
+import net.crookedman.crookedlib.core.animatable.GeoAnimatable;
+import net.crookedman.crookedlib.network.GeckoLibNetwork;
+import net.crookedman.crookedlib.network.SerializableDataTicket;
+import net.crookedman.crookedlib.util.ClientUtils;
+
+import java.util.function.Supplier;
+
+/**
+ * Packet for syncing user-definable animation data for {@link SingletonGeoAnimatable} instances
+ */
+public class AnimDataSyncPacket<D> {
+	private final String syncableId;
+	private final long instanceId;
+	private final SerializableDataTicket<D> dataTicket;
+	private final D data;
+
+	public AnimDataSyncPacket(String syncableId, long instanceId, SerializableDataTicket<D> dataTicket, D data) {
+		this.syncableId = syncableId;
+		this.instanceId = instanceId;
+		this.dataTicket = dataTicket;
+		this.data = data;
+	}
+
+	public void encode(FriendlyByteBuf buffer) {
+		buffer.writeUtf(this.syncableId);
+		buffer.writeVarLong(this.instanceId);
+		buffer.writeUtf(this.dataTicket.id());
+		this.dataTicket.encode(this.data, buffer);
+	}
+
+	public static <D> AnimDataSyncPacket<D> decode(FriendlyByteBuf buffer) {
+		String syncableId = buffer.readUtf();
+		long instanceId = buffer.readVarLong();
+		SerializableDataTicket<D> dataTicket = (SerializableDataTicket<D>)DataTickets.byName(buffer.readUtf());
+		D data = dataTicket.decode(buffer);
+
+		return new AnimDataSyncPacket<>(syncableId, instanceId, dataTicket, data);
+	}
+
+	public void receivePacket(Supplier<NetworkEvent.Context> context) {
+		NetworkEvent.Context handler = context.get();
+
+		handler.enqueueWork(() -> {
+			GeoAnimatable animatable = GeckoLibNetwork.getSyncedAnimatable(this.syncableId);
+
+			if (animatable instanceof SingletonGeoAnimatable singleton)
+				singleton.setAnimData(ClientUtils.getClientPlayer(), this.instanceId, this.dataTicket, this.data);
+		});
+
+		handler.setPacketHandled(true);
+	}
+}
